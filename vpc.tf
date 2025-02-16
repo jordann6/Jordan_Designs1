9b63156d0cb8c66e8f91728d61cd4664fb77061f
Jordan <JordanDiNelson@gmail.com>
2025-02-15 19:16:13 -0600

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-# VPC Setup (Ensure the CIDR block is large enough to accommodate multiple subnets)
+# VPC Setup 
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"  # Expand CIDR range if necessary
 }
@@ -6,25 +6,25 @@ resource "aws_vpc" "main_vpc" {
 # Subnet for EC2 instance
 resource "aws_subnet" "main_subnet_a" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"  # Make sure this block is available
+  cidr_block        = "10.0.1.0/24"  
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "main_subnet_b" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"  # Make sure this block is available
+  cidr_block        = "10.0.2.0/24"  
   availability_zone = "us-east-1b"
 }
 
 # Subnet for RDS 
 resource "aws_subnet" "rds_subnet_a" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.3.0/24"  # Ensure this CIDR block does not conflict
+  cidr_block        = "10.0.3.0/24"  
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "rds_subnet_b" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.4.0/24"  # Ensure this CIDR block does not conflict
+  cidr_block        = "10.0.4.0/24"  
   availability_zone = "us-east-1b"
 }
